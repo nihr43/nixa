9cde87ed241818400d1683a55d719e27550dd655
nathan hensel <nathanihensel@gmail.com>
2025-03-21 14:39:56 -0600

    add static container

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,3 +1,13 @@
 {
   virtualisation.docker.enable = true;
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      nginx = {
+        image = "nginx:latest";
+        ports = [ "80:80" ];
+        autoStart = true;
+      };
+    };
+  };
 }
